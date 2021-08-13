b191e50d1939768e3ac2a5d2ebf5c312fd3f89de
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-08-13 10:10:43 +0700

    Fix cannot load

--- src/main/java/id/universenetwork/utilities/Manager/Proxy.java
+++ src/main/java/id/universenetwork/utilities/Manager/Proxy.java
@@ -1,2 +1,37 @@
-package id.universenetwork.utilities.Manager;public class Proxy {
+package id.universenetwork.utilities.Manager;
+
+import id.universenetwork.utilities.Enums.Features.AddressWhitelister;
+
+public class Proxy {
+    private static String bungeeAddress;
+
+    public static void setup() {
+        setBungeeAddress(Config.AddressWhitelisterMessage(AddressWhitelister.ADDRESS));
+    }
+
+    public static void setBungeeAddress(String bungeeAddress) {
+        Proxy.bungeeAddress = bungeeAddress;
+    }
+
+    public static String getBungeeAddress() {
+        return bungeeAddress;
+    }
+
+    public static Boolean isValidAddress(String s) {
+        if (!s.contains(".")) {
+            return false;
+        } else {
+            String[] v1 = s.split(".");
+            int v2 = v1.length;
+            for (int v3 = 0; v3 < v2; ++v3) {
+                String n = v1[v3];
+                try {
+                    Integer.valueOf(n);
+                } catch (Exception v6) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
