d8b85f87c66e1136335640b550f3509c80ae41ea
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-08-13 10:12:42 +0700

    Fix cannot load

--- src/main/java/id/universenetwork/utilities/Listeners/AddressWhitelisterListener.java
+++ src/main/java/id/universenetwork/utilities/Listeners/AddressWhitelisterListener.java
@@ -1,2 +1,33 @@
-package id.universenetwork.utilities.Listeners;public class AddressWhitelisterListener {
+package id.universenetwork.utilities.Listeners;
+
+import id.universenetwork.utilities.Enums.Features.AddressWhitelister;
+import id.universenetwork.utilities.Manager.Config;
+import id.universenetwork.utilities.Manager.Proxy;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
+
+public class AddressWhitelisterListener implements Listener {
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent e) {
+        if (Config.AddressWhitelisterSettings(AddressWhitelister.ENABLED)) {
+            String kickMSG = Config.AddressWhitelisterMessage(AddressWhitelister.KICKMSG);
+            String joinedAddress = e.getRealAddress().getHostAddress();
+            String bungeeAddress = Proxy.getBungeeAddress();
+            if (bungeeAddress.contains(";")) {
+                String[] v5 = bungeeAddress.split(";");
+                int v6 = v5.length;
+                for (int v7 = 0; v7 < v6; ++v7) {
+                    String address = v5[v7];
+                    if (joinedAddress.equalsIgnoreCase(address)) return;
+                }
+            }
+
+            if (!joinedAddress.equalsIgnoreCase(bungeeAddress)) {
+                kickMSG = kickMSG.replaceAll("%enter%", "\n");
+                e.disallow(Result.KICK_OTHER, kickMSG);
+            }
+        }
+    }
 }
