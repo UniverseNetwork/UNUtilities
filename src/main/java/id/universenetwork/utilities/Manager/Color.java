6bf51ec360fd5dc2a65ce2776f5fc666738c1ad4
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-08-14 08:05:59 +0700

    Delete src/main/java/id/universenetwork/utilities/Manager directory

--- src/main/java/id/universenetwork/utilities/Manager/Color.java
+++
@@ -1,9 +0,0 @@
-package id.universenetwork.utilities.Manager;
-
-import org.bukkit.ChatColor;
-
-public class Color {
-    public static String Translator(String string){
-        return ChatColor.translateAlternateColorCodes('&', string);
-    }
-}
