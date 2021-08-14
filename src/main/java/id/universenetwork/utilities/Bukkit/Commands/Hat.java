f5b90003885b6a296a18158c446ff9c4a088a407
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-08-14 14:31:46 +0700

    Fix Hat Command isn't work

--- src/main/java/id/universenetwork/utilities/Bukkit/Commands/Hat.java
+++ src/main/java/id/universenetwork/utilities/Bukkit/Commands/Hat.java
@@ -1,6 +1,7 @@
 package id.universenetwork.utilities.Bukkit.Commands;
 
 import id.universenetwork.utilities.Bukkit.Enums.Features.HatCommand;
+import id.universenetwork.utilities.Bukkit.Enums.Settings;
 import id.universenetwork.utilities.Bukkit.Manager.Config;
 import id.universenetwork.utilities.Bukkit.Manager.Sender;
 import org.bukkit.command.Command;
@@ -13,22 +14,26 @@ import org.bukkit.inventory.PlayerInventory;
 public class Hat implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!Config.HCSettings(HatCommand.ENABLED)) {
-            Sender.send(sender, Config.HCMessage(HatCommand.DISABLEDMSG));
+        if (sender.hasPermission("unutilities.command.hat")) {
+            if (Config.HCSettings(HatCommand.ENABLED)) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    PlayerInventory inv = p.getInventory();
+                    ItemStack held = inv.getItemInMainHand();
+                    ItemStack helm = inv.getHelmet();
+                    inv.setHelmet(held);
+                    inv.setItemInMainHand(helm);
+                    p.updateInventory();
+                    sender.sendMessage(Config.HCMessage(HatCommand.MESSAGE));
+                } else System.out.println(Config.HCMessage(HatCommand.CONSOLE));
+            } else {
+                Sender.send(sender, Config.HCMessage(HatCommand.DISABLEDMSG));
+                return false;
+            }
+        } else {
+            sender.sendMessage(Config.Settings(Settings.NOPERMISSION));
             return false;
         }
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            PlayerInventory inv = p.getInventory();
-            ItemStack held = inv.getItemInMainHand();
-            ItemStack helm = inv.getHelmet();
-            if (p.hasPermission("unutilities.command.hat")) {
-                inv.setHelmet(held);
-                inv.setItemInMainHand(helm);
-                p.updateInventory();
-                sender.sendMessage(Config.HCMessage(HatCommand.MESSAGE));
-            }
-        } else System.out.println(Config.HCMessage(HatCommand.CONSOLE));
         return true;
     }
 }
