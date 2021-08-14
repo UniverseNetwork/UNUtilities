f7b4979bc7e644c7d4322096dd46481d65108e27
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-08-14 10:18:27 +0700

    Fix all cancelled block placing is marked as redstone components

--- src/main/java/id/universenetwork/utilities/Bukkit/Listeners/AntiRedstoneListener.java
+++ src/main/java/id/universenetwork/utilities/Bukkit/Listeners/AntiRedstoneListener.java
@@ -15,23 +15,30 @@ public class AntiRedstoneListener implements Listener {
             if (Config.ARSettings(AntiRedstone.ENABLED)) {
                 if (m == Material.REDSTONE_WIRE) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.REDSTONE));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.REPEATER) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.REPEATER));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.COMPARATOR) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.COMPARATOR));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.REDSTONE_TORCH) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.REDSTONE_TORCH));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.REDSTONE_WALL_TORCH) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.WALL_REDSTONE_TORCH));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.OBSERVER) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.OBSERVER));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.REDSTONE_BLOCK) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.REDSTONE_BLOCK));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 } else if (m == Material.REDSTONE_LAMP) {
                     e.setCancelled(Config.ARSettings(AntiRedstone.REDSTONE_LAMP));
+                    if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
                 }
             }
-            if (e.isCancelled()) e.getPlayer().sendMessage(Config.ARMessage(AntiRedstone.NOPERMISSION));
         }
     }
 }
