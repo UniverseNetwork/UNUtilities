f067fff2ad6efd9feb40a5601e01580661c15457
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-10-30 05:57:48 +0700

    Optimize Code

--- src/main/java/id/universenetwork/utilities/Bukkit/Hooks/SlimefunAddons/ExtraTools/Implementation/Interfaces/ETInventoryBlock.java
+++ src/main/java/id/universenetwork/utilities/Bukkit/Hooks/SlimefunAddons/ExtraTools/Implementation/Interfaces/ETInventoryBlock.java
@@ -1,24 +1,14 @@
 package id.universenetwork.utilities.Bukkit.Hooks.SlimefunAddons.ExtraTools.Implementation.Interfaces;
 
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
 import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.function.Consumer;
-
-import static io.github.thebusybiscuit.slimefun4.implementation.Slimefun.getPermissionsService;
-import static io.github.thebusybiscuit.slimefun4.implementation.Slimefun.getProtectionManager;
-import static io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction.INTERACT_BLOCK;
 
 public interface ETInventoryBlock {
     int[] getInputSlots();
 
     int[] getOutputSlots();
 
-    default void createPreset(SlimefunItem item, Consumer<BlockMenuPreset> setup) {
+    default void createPreset(io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem item, java.util.function.Consumer<BlockMenuPreset> setup) {
         String title = item.getItemName();
         new BlockMenuPreset(item.getId(), title) {
             @Override
@@ -32,9 +22,8 @@ public interface ETInventoryBlock {
             }
 
             @Override
-            @ParametersAreNonnullByDefault
-            public boolean canOpen(Block b, Player p) {
-                return p.hasPermission("slimefun.inventory.bypass") || getProtectionManager().hasPermission(p, b.getLocation(), INTERACT_BLOCK) && getPermissionsService().hasPermission(p, item);
+            public boolean canOpen(org.bukkit.block.Block b, org.bukkit.entity.Player p) {
+                return p.hasPermission("slimefun.inventory.bypass") || io.github.thebusybiscuit.slimefun4.implementation.Slimefun.getProtectionManager().hasPermission(p, b.getLocation(), io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction.INTERACT_BLOCK) && io.github.thebusybiscuit.slimefun4.implementation.Slimefun.getPermissionsService().hasPermission(p, item);
             }
         };
     }
