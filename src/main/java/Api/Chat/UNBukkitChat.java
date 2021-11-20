c698ac42a9a6726035cae0e80d89d00d95f7d423
rajaopak <ahmadnafis0302@gmail.com>
2021-11-20 21:37:19 +0700

    add simple centered message API

---
+++ src/main/java/Api/Chat/UNBukkitChat.java
@@ -0,0 +1,83 @@
+package Api.Chat;
+
+import Api.Enums.FontEnum;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class UNBukkitChat {
+
+    private final static int CENTER_PX = 154;
+
+    public static void sendCentredMessage(Player player, String message) {
+        if(message == null || message.equals("")) {
+            player.sendMessage("");
+            return;
+        }
+        message = ChatColor.translateAlternateColorCodes('&', message);
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for(char c : message.toCharArray()){
+            if(c == '§'){
+                previousCode = true;
+            }else if(previousCode){
+                previousCode = false;
+                isBold = c == 'l' || c == 'L';
+            }else{
+                FontEnum FI = FontEnum.getFontInfo(c);
+                messagePxSize += isBold ? FI.getBoldLength() : FI.getLength();
+                messagePxSize++;
+            }
+        }
+        int CENTER_PX = 154;
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = FontEnum.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while(compensated < toCompensate){
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+        player.sendMessage(sb.toString() + message);
+    }
+
+    public static String CenteredMessage(String message){
+        String[] lines = ChatColor.translateAlternateColorCodes('&', message).split("\n", 40);
+        StringBuilder returnMessage = new StringBuilder();
+
+
+        for (String line : lines) {
+            int messagePxSize = 0;
+            boolean previousCode = false;
+            boolean isBold = false;
+
+            for (char c : line.toCharArray()) {
+                if (c == '§') {
+                    previousCode = true;
+                } else if (previousCode) {
+                    previousCode = false;
+                    isBold = c == 'l';
+                } else {
+                    FontEnum FI = FontEnum.getFontInfo(c);
+                    messagePxSize = isBold ? messagePxSize + FI.getBoldLength() : messagePxSize + FI.getLength();
+                    messagePxSize++;
+                }
+            }
+            int toCompensate = CENTER_PX - messagePxSize / 2;
+            int spaceLength = FontEnum.SPACE.getLength() + 1;
+            int compensated = 0;
+            StringBuilder sb = new StringBuilder();
+            while(compensated < toCompensate){
+                sb.append(" ");
+                compensated += spaceLength;
+            }
+            returnMessage.append(sb.toString()).append(line).append("\n");
+        }
+
+        return returnMessage.toString();
+    }
+
+}
